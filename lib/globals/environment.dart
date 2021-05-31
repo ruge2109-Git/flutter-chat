645eb04998b0c588c4ba5954e990863b45667e8b
Jonathan Ruge <53587500+ruge2109-Git@users.noreply.github.com>
2021-05-31 12:03:25 -0500

    Cambio en la ruta del api

--- lib/globals/environment.dart
+++ lib/globals/environment.dart
@@ -1,4 +1,6 @@
 class Environments {
-  static String apiUrl = "http://192.168.0.103:3000/api";
-  static String socketUrl = "http://192.168.0.103:3000/";
+  // static String apiUrl = "http://192.168.0.103:3000/api";
+  // static String socketUrl = "http://192.168.0.103:3000/";
+  static String apiUrl = "https://chatbackendruge.herokuapp.com/api";
+  static String socketUrl = "https://chatbackendruge.herokuapp.com/";
 }
